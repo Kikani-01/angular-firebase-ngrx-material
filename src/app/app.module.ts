630a99cd6408e2091ce262ea5169e1e11e6f124f
Kikani-01 <kikmdev99@gmail.com>
2023-01-11 23:31:21 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { AppRoutingModule } from './app-routing.module';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environments';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyCpbaL16SDF3bwhcDkXtxI2KZrS3VDSJkc',
+    authDomain: 'angular-test-demo-e64d8.firebaseapp.com',
+    projectId: 'angular-test-demo-e64d8',
+    storageBucket: 'angular-test-demo-e64d8.appspot.com',
+    messagingSenderId: '192126397204',
+    appId: '1:192126397204:web:c0a407fd529261bf51f001',
+    measurementId: 'G-1PXK0FMGLJ',
+  },
+  production: true,
+};
